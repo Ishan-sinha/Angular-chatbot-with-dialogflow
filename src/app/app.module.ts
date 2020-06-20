f9ec0aba2c2e4646e432d7371e979f2446e50a8b
Ishan Sinha <ishan.sinha1998@gmail.com>
2020-06-20 09:26:43 +0530

    Add source code

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { ChatModule } from './chat/chat.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChatModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
